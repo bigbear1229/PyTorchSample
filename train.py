7702f0ced1aa6a1b2c208388a5c7b1cd016f6a5f
bigbear1229 <bigbear1229@outlook.com>
2026-09-11 10:07:27 +0800

    Update model information and remove contact note

--- train.py
+++ train.py
@@ -154,7 +154,13 @@ def train_model(
                                 },
                                 'step': global_step,
                                 'epoch': epoch,
-                                'stop' : 'ask gyonghak why cannot contact, and ask rimyongjun for the password to the mokrok.rar file',
+                                'model' : '1.What is the width of the watch band connection part? 18mm? 19mm? ...?
+                                        2.The models that are not available are as follows. Please choose again and send. The model format to be resent is "type-brand-number-price."
+                                        bag:COMICO-64
+                                        dress:yiweine-6,danmunier-46,danmunier-97,shixu-164
+                                        short shirt:jingdiangushi-3,pierkadan-42
+                                        long shirt:LIME-51,SETUBILA-170,SETUBILA-429,hengyuanxiang-176,demanei-12
+                                        pajamas:fentengkean-133,fentengkean-515',
                                 **histograms
                             })
                         except:
